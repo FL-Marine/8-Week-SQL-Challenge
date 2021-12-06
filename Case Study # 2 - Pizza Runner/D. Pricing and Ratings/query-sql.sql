d86efc9c9488145310e27e11d168a6fef9269f29
Abe Diaz <74512335+FL-Marine@users.noreply.github.com>
2021-12-06 05:04:36 -0500

    Update query-sql.sql

--- Case Study # 2 - Pizza Runner/D. Pricing and Ratings/query-sql.sql
+++ Case Study # 2 - Pizza Runner/D. Pricing and Ratings/query-sql.sql
@@ -4,3 +4,15 @@ SELECT
   data_type
 FROM information_schema.columns
 WHERE table_name = 'customer_orders';
+
+--Result:
++──────────────────+──────────────+──────────────────────────────+
+| table_name       | column_name  | data_type                    |
++──────────────────+──────────────+──────────────────────────────+
+| customer_orders  | order_id     | integer                      |
+| customer_orders  | customer_id  | integer                      |
+| customer_orders  | pizza_id     | integer                      |
+| customer_orders  | exclusions   | character varying            |
+| customer_orders  | extras       | character varying            |
+| customer_orders  | order_time   | timestamp without time zone  |
++──────────────────+──────────────+──────────────────────────────+
